4bb318ed05d61c4e162bd46fc7cf03ca9ff67eae
alastairUK <alastairUK@users.noreply.github.com>
2019-10-31 16:50:55 +0000

    Fix issues with DLLEXPORT / add pragma once

--- include/p528.h
+++ include/p528.h
@@ -1,4 +1,14 @@
+#pragma once
+
+// Operating system preprocessor directives *********************************************************
+
+// Define DLLEXPORT for any platform
+#ifdef _WIN32
 #define DLLEXPORT extern "C" __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
