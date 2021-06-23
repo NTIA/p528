6277f69de5e67e557a14fc117c5df9e7b70f1a3b
Billy Kozma <wkozma@ntia.doc.gov>
2021-06-23 16:01:09 -0600

    added polarization enum and overloaded method to use it

--- dotnet/ITS.Propagation.P528/P528.cs
+++ dotnet/ITS.Propagation.P528/P528.cs
@@ -8,6 +8,22 @@ namespace ITS.Propagation
     /// </summary>
     public static partial class P528
     {
+        /// <summary>
+        /// Polarization
+        /// </summary>
+        public enum Polarization : int
+        {
+            /// <summary>
+            /// Horizontal polarization
+            /// </summary>
+            Horizontal = 0,
+
+            /// <summary>
+            /// Vertical polarization
+            /// </summary>
+            Vertical = 1
+        }
+
         private const string P528_x86_DLL_NAME = "p528_x86.dll";
         private const string P528_x64_DLL_NAME = "p528_x64.dll";
 
@@ -89,6 +105,23 @@ namespace ITS.Propagation
         /// <param name="T_pol">Polarization</param>
         /// <param name="p">Time percentage (0 &lt; q &lt; 1)</param>
         /// <param name="result">Result data structure</param>
+        /// <returns>Return code</returns>
+        public static int Invoke(double d__km, double h_1__meter, double h_2__meter, double f__mhz, Polarization T_pol,
+            double p, out Result result)
+        {
+            return P528_Invoke(d__km, h_1__meter, h_2__meter, f__mhz, (int)T_pol, p, out result);
+        }
+
+        /// <summary>
+        /// Recommendation ITU-R P.528-5
+        /// </summary>
+        /// <param name="d__km">Path distance, in km</param>
+        /// <param name="h_1__meter">Height of the low terminal, in meters</param>
+        /// <param name="h_2__meter">Height of the high terminal, in meters</param>
+        /// <param name="f__mhz">Frequency, in MHz</param>
+        /// <param name="T_pol">Polarization</param>
+        /// <param name="p">Time percentage (0 &lt; q &lt; 1)</param>
+        /// <param name="result">Result data structure</param>
         /// <param name="terminal_1">Intermediate values for terminal 1 geometry</param>
         /// <param name="terminal_2">Intermediate values for terminal 2 geometry</param>
         /// <param name="tropo">Intermediate values for troposcatter calculations</param>
@@ -99,6 +132,29 @@ namespace ITS.Propagation
             out Terminal terminal_1, out Terminal terminal_2, out TroposcatterParams tropo, out Path path, out LineOfSightParams los_params)
         {
             return P528Ex_Invoke(d__km, h_1__meter, h_2__meter, f__mhz, T_pol, p, out result, out terminal_1, 
+                out terminal_2, out tropo, out path, out los_params);
+        }
+
+        /// <summary>
+        /// Recommendation ITU-R P.528-5
+        /// </summary>
+        /// <param name="d__km">Path distance, in km</param>
+        /// <param name="h_1__meter">Height of the low terminal, in meters</param>
+        /// <param name="h_2__meter">Height of the high terminal, in meters</param>
+        /// <param name="f__mhz">Frequency, in MHz</param>
+        /// <param name="T_pol">Polarization</param>
+        /// <param name="p">Time percentage (0 &lt; q &lt; 1)</param>
+        /// <param name="result">Result data structure</param>
+        /// <param name="terminal_1">Intermediate values for terminal 1 geometry</param>
+        /// <param name="terminal_2">Intermediate values for terminal 2 geometry</param>
+        /// <param name="tropo">Intermediate values for troposcatter calculations</param>
+        /// <param name="path">Intermediate values for propagation path</param>
+        /// <param name="los_params">Intermediate values for Line-of-Sight calculations</param>
+        /// <returns>Return code</returns>
+        public static int InvokeEx(double d__km, double h_1__meter, double h_2__meter, double f__mhz, Polarization T_pol, double p, out Result result,
+            out Terminal terminal_1, out Terminal terminal_2, out TroposcatterParams tropo, out Path path, out LineOfSightParams los_params)
+        {
+            return P528Ex_Invoke(d__km, h_1__meter, h_2__meter, f__mhz, (int)T_pol, p, out result, out terminal_1,
                 out terminal_2, out tropo, out path, out los_params);
         }
     }
