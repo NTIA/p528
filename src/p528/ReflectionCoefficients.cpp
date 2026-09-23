3d2e8cc1a8000a83cb8790266f2884070743e2f3
mcutkoskyNTIA <mcutkosky@ntia.gov>
2026-09-23 11:11:17 -0600

    Update ReflectionCoefficients.cpp

--- src/p528/ReflectionCoefficients.cpp
+++ src/p528/ReflectionCoefficients.cpp
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "p528.h"
 
-/*******************************************************************************
+/**
  * Computes the reflection coefficients
  *
  * References:
